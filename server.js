5558137c1c040538722abdb67d8659d4e6de8ad9
Tushar Nagle <140832814+tushar93026@users.noreply.github.com>
2025-10-27 00:15:58 +0530

    2ndCOMMIT

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const port = process.env.PORT || 4000
 connectDB()
 connectCloudinary()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: ['https://frontend-doctor-mu.vercel.app/'], // your Vercel domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use('/api/admin', adminRouter)
 app.use('/api/doctor', doctorRouter)
 app.use('/api/user', userRouter)
